50d7515f58bf4ae0acf11c47a23200b6ede880b4
Priyobrato Das <priyodas12@gmail.com>
2020-11-05 10:45:49 +0530

    added all files

---
+++ src/main/java/io/springlab/awscicdspringbootinit/AwsCicdSpringbootInitApplication.java
@@ -0,0 +1,13 @@
+package io.springlab.awscicdspringbootinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicdSpringbootInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCicdSpringbootInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/springlab/awscicdspringbootinit/AwsCicdSpringbootInitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springlab.awscicdspringbootinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCicdSpringbootInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
